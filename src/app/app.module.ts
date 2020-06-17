f864b006b70a59df2dd6c49b1e7e45f862f4deb8
Bhairesh <Bhairesh@arithaconsulting.com>
2020-06-17 17:50:47 +0530

    Project Structure changed.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AuthComponent } from './auth/auth.component';
-import { LoginComponent } from './auth/login/login.component';
-import { SignupComponent } from './auth/signup/signup.component';
-import { ForgotPasswordComponent } from './auth/forgot-password/forgot-password.component';
-import { ResetPasswordComponent } from './auth/reset-password/reset-password.component';
-import { AdminComponent } from './admin/admin.component';
-import { UsersComponent } from './users/users.component';
-import { OrphanagesComponent } from './orphanages/orphanages.component';
-import { OrphanagesListComponent } from './orphanages/orphanages-list/orphanages-list.component';
-import { DonateComponent } from './donate/donate.component';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-import { HomeComponent } from './home/home.component';
+import { AuthComponent } from './modules/auth/auth.component';
+import { LoginComponent } from './modules/auth/login/login.component';
+import { SignupComponent } from './modules/auth/signup/signup.component';
+import { ForgotPasswordComponent } from './modules/auth/forgot-password/forgot-password.component';
+import { ResetPasswordComponent } from './modules/auth/reset-password/reset-password.component';
+import { AdminComponent } from './modules/admin/admin.component';
+import { UsersComponent } from './modules/users/users.component';
+import { OrphanagesComponent } from './modules/orphanages/orphanages.component';
+import { OrphanagesListComponent } from './modules/orphanages/orphanages-list/orphanages-list.component';
+import { DonateComponent } from './modules/donate/donate.component';
+import { PageNotFoundComponent } from './modules/page-not-found/page-not-found.component';
+import { HomeComponent } from './modules/home/home.component';
 
 @NgModule({
   declarations: [
