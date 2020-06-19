93f4154e07e0b580febd69393257097f60ff1738
Bhairesh <Bhairesh@arithaconsulting.com>
2020-06-19 18:39:26 +0530

    Icons, Favicons added.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -38,7 +39,8 @@ import { FooterComponent } from './modules/home/footer/footer.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/modules/home/header/header.component.ts
+++ src/app/modules/home/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, NavigationStart, NavigationEnd } from '@angular/router';
+import { Location, PopStateEvent } from '@angular/common';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +9,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  public isCollapsed = true;
+  private lastPoppedUrl: string;
+  private yScrollStack: number[] = [];
+
+  constructor(public location: Location, private router: Router) {
+  }
 
   ngOnInit() {
+    this.router.events.subscribe((event) => {
+      this.isCollapsed = true;
+      if (event instanceof NavigationStart) {
+        if (event.url != this.lastPoppedUrl)
+          this.yScrollStack.push(window.scrollY);
+      } else if (event instanceof NavigationEnd) {
+        if (event.url == this.lastPoppedUrl) {
+          this.lastPoppedUrl = undefined;
+          window.scrollTo(0, this.yScrollStack.pop());
+        } else
+          window.scrollTo(0, 0);
+      }
+    });
+    this.location.subscribe((ev: PopStateEvent) => {
+      this.lastPoppedUrl = ev.url;
+    });
+  }
+
+  isHome() {
+    var titlee = this.location.prepareExternalUrl(this.location.path());
+
+    if (titlee === '/home') {
+      return true;
+    }
+    else {
+      return false;
+    }
   }
 
 }
